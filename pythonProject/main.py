ac53e121d18c2100a62ab5b5f8263aef666138c8
Oleg <89175955650@mail.ru>
2024-03-15 13:58:52 +0300

    Next commit: log.log, main3, proxy.py.

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,7 +1,9 @@
+import random
 import webbrowser
 import time
 import datetime
 from selenium import webdriver
+import logging
 
 driver = webdriver.Chrome()
 file_name = "history.txt"
@@ -10,8 +12,11 @@ links_name = ["https://kvartiravkaluge.ru/", "https://domvkaluge.ru/", "https://
 
 
 def write_file(text):
-    with open(file_name, "a") as file:
-        file.write(text)
+    logging.basicConfig(level=logging.INFO, filename="log.log", filemode="w",
+                        format="%(asctime)s %(levelname)s %(message)s")
+    logging.info(text)
+    # with open(file_name, "a") as file:
+    #     file.write(text)
 
 
 def sleep_func(time_sec):
@@ -22,7 +27,7 @@ def open_link(links_name):
     # webbrowser.open_new_tab(links_name)
     driver.get(links_name)
     result_text = f"open link: {links_name} in {datetime.datetime.now()}"
-    print(result_text)
+    print(result_text[:-7])
     write_file(result_text)
 
 
@@ -31,15 +36,27 @@ def close_link():
 
 
 def main():
-    for times in range(3):
+    total = 1
+    start_time = time.time()
+    start = datetime.datetime.now()
+    print(f"Start: {start}."[:-8])
+    for times in range(total):
         print(f"{times + 1} period!")
         for link in links_name:
             open_link(link)
             sleep_func(time_sec=1)
             close_link()
             result_txt = f"close link {link} in {datetime.datetime.now()}"
-            print(result_txt)
-            write_file("\n" + result_txt + "\n")
+            print(result_txt[:-7])
+            write_file(result_txt + "\n")
+        if times == total - 1:
+            end = datetime.datetime.now()
+            end_time = time.time()
+            total_time = end_time - start_time
+            process_time = datetime.timedelta(seconds=total_time)
+            print(f"End: {end}."[:-8])
+            print(f"Total process time is: {process_time}."[:-8])
+    sleep_func(time_sec=random.choice([1, 2, 3, 4, 5]))
 
 
 if __name__ == '__main__':
--- pythonProject/main3.py
+++ pythonProject/main3.py
@@ -6,26 +6,36 @@ from ipaddress import IPv4Network
 import random
 from pprint import pprint
 from selenium import webdriver
+import datetime
 
 
 def main():
+    ip_http = "110.17.3.65"
+    port_http = "3128"
+    ip_https = "65.21.188.18"
+    port_https = "8080"
     url = "https://uchastok40.ru/"
     proxies = {
-        "http": "http://127.0.0.1:8000",  # change the real proxy
-        "https": "https://127.09.0.10:8080"  # change the real proxy
+        "http": f"http://{ip_http}:{port_http}",  # change the real proxy
+        # "https": f"https://{ip_https}:{port_https}"  # change the real proxy
     }
-    ip = random.choice([str(ip) for ip in IPv4Network("192.0.2.0/23")])
+    # ip = random.choice([str(ip) for ip in IPv4Network("192.0.2.0/23")])
     ua = UserAgent()
     header = {
         "User-Agent": ua.random
     }
-    pprint(ip)
+    driver = webdriver.Chrome()
+    driver.get(url=url)
+    print(f"Link:{url}---IP:{ip_http}------{datetime.datetime.now()}")
+    # print(ip_http, ip_https)
     # pprint(ua)
     # pprint(header)
+    time.sleep(10)
     response = requests.get(url=url, headers=header, proxies=proxies)
     pprint(response.status_code)
     # pprint(response.text)
-    time.sleep(5)
+    time.sleep(30)
+    driver.quit()
 
 
 if __name__ == "__main__":
